b969601c1227bedb7a6bf808ddb8273290da3818
jeffrey chen <jeffreychen2016@gmail.com>
2018-02-27 18:57:37 -0600

    Added the last practice

---
+++ strings.js
@@ -0,0 +1,46 @@
+// console.log('Hello!');
+
+//Challenge #1
+var numOfSandwitches = 4 * 2;
+console.log(numOfSandwitches);
+
+//Challenge #2
+var name = "Jeffrey";
+var string = "Hello, " + name + " !";
+console.log("string:",string);
+
+//Challenge #3
+
+var DNA = "GCAT";
+var RNA = DNA.replace("T","U");
+console.log("RNA:", RNA);
+
+//Challenge #4
+var animal = "alligator";
+// var animal = prompt();
+var animal = "Dog";
+// var animal = "dog";
+if(animal.toLowerCase() === "alligator"){
+    // console.log("Big");
+    // debugger;
+}
+else{
+    console.log("Small");
+}
+
+var yarn = "the better string";
+var domString = "<h4>" + yarn + "</h4>";
+
+var myDiv = document.getElementById('yarn-holder');
+
+// myDiv.innerHTML = domString;
+
+var str = 'IBMELOVEbmcatsbmw';
+// str = str.replace(/b/ig,'').replace(/m/ig,'').replace(/w/ig,'');
+str = str.replace(/[bmw]/ig,'');
+var domString = '<h>' + str + '</h>';
+var myH1 = document.getElementById('output');
+myH1.innerHTML = domString;
+
+
+
